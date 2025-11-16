8d4b249c9739033c4c0a66bea4baa82d3b2d141c
A K Verma <amitkkc01@gmail.com>
2025-11-16 17:28:39 +0530

    stg code edited

--- modules/storage/azurerm_storage_account/variables.tf
+++ modules/storage/azurerm_storage_account/variables.tf
@@ -176,56 +176,53 @@
 # }
 
 variable "stgaccount" {
-  description = "Map of storage accounts with optional settings"
+  description = "A map of storage accounts to create"
   type = map(object({
 
-    # Required
     name                     = string
     location                 = string
     rg_key                   = string
     account_tier             = string
     account_replication_type = string
 
-    # Optional arguments
-    account_kind                      = optional(string, null)
-    provisioned_billing_model_version = optional(string, null)
-    tags                              = optional(map(string), null)
-    cross_tenant_replication_enabled  = optional(bool, null)
-    access_tier                       = optional(string, null)
-    edge_zone                         = optional(string, null)
-    https_traffic_only_enabled        = optional(bool, null)
-    min_tls_version                   = optional(string, null)
-    allow_nested_items_to_be_public   = optional(bool, null)
-    shared_access_key_enabled         = optional(bool, null)
-    public_network_access_enabled     = optional(bool, null)
-    default_to_oauth_authentication   = optional(bool, null)
-    is_hns_enabled                    = optional(bool, null)
-    nfsv3_enabled                     = optional(bool, null)
-    large_file_share_enabled          = optional(bool, null)
-    local_user_enabled                = optional(bool, null)
-    queue_encryption_key_type         = optional(string, null)
-    table_encryption_key_type         = optional(string, null)
-    infrastructure_encryption_enabled = optional(bool, null)
-    allowed_copy_scope                = optional(string, null)
-    sftp_enabled                      = optional(bool, null)
-    dns_endpoint_type                 = optional(string, null)
+    account_kind                      = optional(string)
+    provisioned_billing_model_version = optional(string)
+    tags                              = optional(map(string))
+    cross_tenant_replication_enabled  = optional(bool)
+    access_tier                       = optional(string)
+    edge_zone                         = optional(string)
+    https_traffic_only_enabled        = optional(bool)
+    min_tls_version                   = optional(string)
+    allow_nested_items_to_be_public   = optional(bool)
+    shared_access_key_enabled         = optional(bool)
+    public_network_access_enabled     = optional(bool)
+    default_to_oauth_authentication   = optional(bool)
+    is_hns_enabled                    = optional(bool)
+    nfsv3_enabled                     = optional(bool)
+    large_file_share_enabled          = optional(bool)
+    local_user_enabled                = optional(bool)
+    queue_encryption_key_type         = optional(string)
+    table_encryption_key_type         = optional(string)
+    infrastructure_encryption_enabled = optional(bool)
+    allowed_copy_scope                = optional(string)
+    sftp_enabled                      = optional(bool)
+    dns_endpoint_type                 = optional(string)
 
-    # Optional blocks
     custom_domain = optional(object({
       name          = string
-      use_subdomain = optional(bool, null)
-    }), null)
+      use_subdomain = optional(bool)
+    }))
 
     customer_managed_key = optional(object({
       user_assigned_identity_id = string
-      key_vault_key_id          = optional(string, null)
-      managed_hsm_key_id        = optional(string, null)
-    }), null)
+      key_vault_key_id          = optional(string)
+      managed_hsm_key_id        = optional(string)
+    }))
 
     identity = optional(object({
       type         = string
       identity_ids = list(string)
-    }), null)
+    }))
 
     blob_properties = optional(object({
       cors_rule = optional(list(object({
@@ -234,26 +231,26 @@ variable "stgaccount" {
         allowed_origins    = list(string)
         exposed_headers    = list(string)
         max_age_in_seconds = number
-      })), null)
+      })))
 
       delete_retention_policy = optional(object({
         days = number
-      }), null)
+      }))
 
       restore_policy = optional(object({
         days = number
-      }), null)
+      }))
 
-      versioning_enabled            = optional(bool, null)
-      change_feed_enabled           = optional(bool, null)
-      change_feed_retention_in_days = optional(number, null)
-      default_service_version       = optional(string, null)
-      last_access_time_enabled      = optional(bool, null)
+      versioning_enabled            = optional(bool)
+      change_feed_enabled           = optional(bool)
+      change_feed_retention_in_days = optional(number)
+      default_service_version       = optional(string)
+      last_access_time_enabled      = optional(bool)
 
       container_delete_retention_policy = optional(object({
-        days = optional(number, null)
-      }), null)
-    }), null)
+        days = optional(number)
+      }))
+    }))
 
     queue_properties = optional(object({
       cors_rule = optional(list(object({
@@ -262,35 +259,35 @@ variable "stgaccount" {
         allowed_origins    = list(string)
         exposed_headers    = list(string)
         max_age_in_seconds = number
-      })), null)
+      })))
 
       logging = optional(object({
         delete                = bool
         read                  = bool
         write                 = bool
         version               = string
-        retention_policy_days = optional(number, null)
-      }), null)
+        retention_policy_days = optional(number)
+      }))
 
       hour_metrics = optional(object({
         enabled               = bool
         version               = string
-        include_apis          = optional(bool, null)
-        retention_policy_days = optional(number, null)
-      }), null)
+        include_apis          = optional(bool)
+        retention_policy_days = optional(number)
+      }))
 
       minute_metrics = optional(object({
         enabled               = bool
         version               = string
-        include_apis          = optional(bool, null)
-        retention_policy_days = optional(number, null)
-      }), null)
-    }), null)
+        include_apis          = optional(bool)
+        retention_policy_days = optional(number)
+      }))
+    }))
 
     static_website = optional(object({
-      index_document     = optional(string, null)
-      error_404_document = optional(string, null)
-    }), null)
+      index_document     = optional(string)
+      error_404_document = optional(string)
+    }))
 
     share_properties = optional(object({
       cors_rule = optional(list(object({
@@ -299,11 +296,11 @@ variable "stgaccount" {
         allowed_origins    = list(string)
         exposed_headers    = list(string)
         max_age_in_seconds = number
-      })), null)
+      })))
 
       retention_policy = optional(object({
         days = number
-      }), null)
+      }))
 
       smb = optional(object({
         versions                        = list(string)
@@ -311,54 +308,57 @@ variable "stgaccount" {
         kerberos_ticket_encryption_type = string
         channel_encryption_type         = string
         multichannel_enabled            = bool
-      }), null)
-    }), null)
+      }))
+    }))
 
     network_rules = optional(object({
       default_action             = string
-      bypass                     = optional(list(string), null)
-      virtual_network_subnet_ids = optional(list(string), null)
-      ip_rules                   = optional(list(string), null)
+      bypass                     = optional(list(string))
+      virtual_network_subnet_ids = optional(list(string))
+      ip_rules                   = optional(list(string))
 
       private_link_access = optional(object({
         endpoint_resource_id = string
-        endpoint_tenant_id   = optional(string, null)
-      }), null)
-    }), null)
+        endpoint_tenant_id   = optional(string)
+      }))
+    }))
 
     azure_files_authentication = optional(object({
-      directory_type                 = optional(string, null)
-      default_share_level_permission = optional(string, null)
+      directory_type                 = optional(string)
+      default_share_level_permission = optional(string)
 
       active_directory = object({
-        domain_name         = optional(string, null)
-        netbios_domain_name = optional(string, null)
-        forest_name         = optional(string, null)
-        domain_guid         = optional(string, null)
-        domain_sid          = optional(string, null)
-        storage_sid         = optional(string, null)
+        domain_name         = optional(string)
+        netbios_domain_name = optional(string)
+        forest_name         = optional(string)
+        domain_guid         = optional(string)
+        domain_sid          = optional(string)
+        storage_sid         = optional(string)
       })
-    }), null)
+    }))
 
     routing = optional(object({
       choice                      = string
       publish_microsoft_endpoints = bool
       publish_internet_endpoints  = bool
-    }), null)
+    }))
 
     immutability_policy = optional(object({
       state                         = string
       allow_protected_append_writes = bool
       period_since_creation_in_days = number
-    }), null)
+    }))
 
     sas_policy = optional(object({
       expiration_period = string
       expiration_action = string
-    }), null)
+    }))
 
   }))
+
+  default = {}
 }
+
 
 variable "rg_names" {
   description = "Map of RG names from RG module"
